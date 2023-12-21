3854edbaf0c74b9ee98aeaea2549f1839d718914
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-12-21 16:39:02 +0200

    Refactor: Code analyzing project

--- 02_AnalyzingCodeTreeStructure/Splitting a code to its components/Program.cs
+++ 02_AnalyzingCodeTreeStructure/Splitting a code to its components/Program.cs
@@ -10,41 +10,55 @@ namespace CodeAnalyzer
         {
             string code = "using text; \nnamespace amad \n {/*comment*/ public partial class heelo {" +
                 "int pp;" +
-                "} \n}"; // Your code here
+                "} \n}"; // Code Sample
+            ProcessCSharpCode(code);
 
+        }
+        /// <summary>
+        /// Parses the provided C# code into a syntax tree and extracts various elements.
+        /// </summary>
+        /// <param name="code">The C# code to be parsed.</param>
+        public static void ProcessCSharpCode(string code)
+        {
+            // Parse the code into a syntax tree
             SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
             CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
 
-            // Get all using directives
+            // Get all using directives within the code
             IEnumerable<UsingDirectiveSyntax> usingDirectives = root.DescendantNodes().OfType<UsingDirectiveSyntax>();
 
+            // Combine all using directives into a single string
             string usings = string.Join("\n", usingDirectives.Select(u => u.ToFullString()));
 
-            // Get all namespace declarations
+            /// Get all namespace declarations within the code
             IEnumerable<NamespaceDeclarationSyntax> namespaceDeclarations = root.DescendantNodes().OfType<NamespaceDeclarationSyntax>();
 
             foreach (var namespaceDeclaration in namespaceDeclarations)
             {
-                // Get all class declarations
+                // Get all class declarations within each namespace
                 IEnumerable<ClassDeclarationSyntax> classDeclarations = namespaceDeclaration.DescendantNodesAndSelf().OfType<ClassDeclarationSyntax>();
                 foreach (var classDeclaration in classDeclarations)
                 {
+                    // Write the class content to a file named after the class
                     File.WriteAllText($"{classDeclaration.Identifier.Text}.cs", $"{usings}\nnamespace {namespaceDeclaration.Name} {{ {classDeclaration.ToFullString()} }}");
                 }
 
-                // Get all interface declarations
+                // Get all interface declarations within each namespace
                 IEnumerable<InterfaceDeclarationSyntax> interfaceDeclarations = namespaceDeclaration.DescendantNodes().OfType<InterfaceDeclarationSyntax>();
                 foreach (var interfaceDeclaration in interfaceDeclarations)
                 {
+                    // Write the interface content to a file named after the interface
                     File.WriteAllText($"{interfaceDeclaration.Identifier.Text}.cs", $"{usings}\nnamespace {namespaceDeclaration.Name} {{ {interfaceDeclaration.ToFullString()} }}");
                 }
 
-                // Get all struct declarations
+                // Get all struct declarations within each namespace
                 IEnumerable<StructDeclarationSyntax> structDeclarations = namespaceDeclaration.DescendantNodes().OfType<StructDeclarationSyntax>();
                 foreach (var structDeclaration in structDeclarations)
                 {
+                    // Write the struct content to a file named after the struct
                     File.WriteAllText($"{structDeclaration.Identifier.Text}.cs", $"{usings}\nnamespace {namespaceDeclaration.Name} {{ {structDeclaration.ToFullString()} }}");
                 }
+
                 Console.WriteLine("Done!");
             }
         }
