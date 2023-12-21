2cafe3b00a36fca02d7acfa706ecbc4f40755598
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-12-21 16:39:45 +0200

    Add: Source Generation Lesson Example

---
+++ 01_SourceGenerators/Simple_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Simple_HelloWorld
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFrom("Generated Code");
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
